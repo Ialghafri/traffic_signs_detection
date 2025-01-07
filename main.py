50eebb0d4191ea932bb66e6fbaf985015d04be06
Ibrahim Al Ghafri <ibrahim.ghafri@rihal.om>
2025-01-07 10:03:23 +0400

    Added Main Entry Point using uvicorn run to main.py

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
