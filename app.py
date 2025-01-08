c3119b1b53a464b9c14bbd1a40453a09ff216034
Ibrahim Al Ghafri <ibrahim.ghafri@rihal.om>
2025-01-08 11:40:25 +0400

    Removed Use Column Width warning by using column based layouts for both image displays

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import requests
 from PIL import Image
 import matplotlib.pyplot as plt
 from io import BytesIO
-#from main import result
-#from main import annotated_image
 import os
 from main import predict
 
@@ -19,7 +17,10 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpeg", "jpg", "png"])
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    c1, c2, c3 = st.columns([0.1,10,0.1])
+    with c2:
+        st.image(image, caption="Uploaded Image", use_container_width=True)
+    
 
     # Send image to backend
     with st.spinner("Classifying..."):
@@ -28,15 +29,9 @@ if uploaded_file is not None:
         if response.status_code == 200:
             result = response.json()
             st.write(f"Predicted Classes: {result['predicted_classes']}")
-            st.image("annotated-images/annotated_file", caption="Annotated Image with Bounding Boxes", use_container_width=True)
-            #annotated_image_response = requests.get(f"{FASTAPI_URL}/predict/annotated-image")
-
-            # if annotated_image_response.status_code == 200:
-            #     annotated_image = Image.open(BytesIO(annotated_image_response.content))
-            #     st.image(annotated_image, caption="Annotated Image with Bounding Boxes", use_column_width=True)
-            # else:
-            #     st.error("Failed to load annotated image.")
-
+            c1, c2, c3 = st.columns([0.1,10,0.1])
+            with c2:
+                st.image("annotated-images/annotated_file", caption="Annotated Image with Bounding Boxes", use_container_width=True)
             
         else:
             st.error(f"Error: {response.json()['error']}")
@@ -45,52 +40,7 @@ if uploaded_file is not None:
 
 
 
-            # image = read_imagefile(file.read())
 
-            # annotated_image_response = requests.get("http://127.0.0.1:8000/predict/annotated-image")
-            # annotated_image = Image.open(BytesIO(annotated_image_response.content))
-            # st.image(annotated_image, caption="annotated Image with Bounding Boxes", use_column_width = True)
-
-
-
-#      request = requests.get("http://127.0.0.1:8000/predict/", files={"result": file.getvalue()})
-#             # Show the annotated image
-#           st.image(annotated_image, caption="Annotated Image with Bounding Boxes", use_column_width=True)
-
-
-# #upload
-# if uploaded_file is not None:
-
-#     image = Image.open(uploaded_file)
-
-#     st.image(image, caption="uploaded image or video", use_column_width= True)
-#     #st.write("Classifying...")
-
-#     #st.write(f"Predicted Classes: {predicted_class_names}")
-
-#     with st.spinner("Classifying..."):
-#         files = {"file": uploaded_file.getvalue()}
-#         response = requests.post(FASTAPI_URL, files=files)
-
-#     prediction_response = requests.get("http://127.0.0.1:8000/predict/")
-
-#     # annotated image is generated with bounding boxes
-#     annotated_image = result.plot()  # use result.plot() to get the annotated image
-    
-#     # show the annotated image
-#     st.image(annotated_image, caption="Annotated Image with Bounding Boxes", use_column_width=True)
-
-#     # Handle response
-#     if response.status_code == 200:
-#         predictions = response.json().get("predicted_classes", [])
-#         annotated_image = response.json().get("annotated_image", [])
-
-#         st.write(f"Predicted Classes: {predictions}")
-#     else:
-#         st.error(f"Error: {response.json().get('error', 'Unknown error')}")
-
-    # # Send POST request to FastAPI
-    # response = requests.post(FASTAPI_URL, files={"file": (result, annotated_image, predicted_class_names})
 
         
 
--- main.py
+++ main.py
@@ -10,11 +10,10 @@ import cv2
 import subprocess
 
 
-
 app = FastAPI()
 
 def load_model():
-    model = YOLO('runs2/detect/train2/weights/best.pt')  # , weights_only=False
+    model = YOLO('runs2/detect/train2/weights/best.pt')  
     model.eval()
     return model 
 
@@ -65,59 +64,8 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
+
+
 # Imporvements 
 # -- import subprocess - include reload= True | "--reload"
 # -- c1, c2, c3 = st.columns[(1,3,1)]
-
-# ---- Excess code -----
-
-# def read_imagefile(file: bytes) -> Image.Image:
-#     try:
-#         image = Image.open(uploaded_file)
-#         return Image.open(BytesIO(file)).convert("RGB")
-#     except Exception as e:
-#         raise ValueError(f"Failed to read image file: {str(e)}")
-
-
-#       return JSONResponse(content=response)
-
-# @app.get("/predict/annotated-image")
-# async def get_annotated_image():
-#     try:
-#         buffer = app.state.annotated_image_buffer
-#         return FileResponse(buffer, media_type="image/jpeg", filename="annotated_image.jpg")
-#     except AttributeError:
-#         return JSONResponse(content={"error": "Annotated image not found. Make sure to call /predict/ first."}, status_code=404)
-
-
-# convert to base
-
-# response2 = {"annotated_image": imagee}
-# JSONResponse(content=response), FileResponse(response2)
-
-# @app.post("/predict/")
-# async def predict(file: UploadFile):
-
-#     contents = await file.read()
-
-#     #image = Image.open(io.BytesIO(contents))
-
-#     image = Image.open(uploaded_file)
-
-#     #predict(uploaded_file, model)
-
-#     prediction = model.predict(source=image, save=True, save_dir='inference_results')
-
-#     result = prediction[0]  # First image's prediction
-
-#     # Get predicted class names and class IDs
-#     predicted_classes = result.names  # This contains the class names
-#     predicted_labels = result.boxes.cls  # The indices of predicted classes (labels)
-#     # Map predicted labels to class names
-#     predicted_class_names = [predicted_classes[int(label)] for label in predicted_labels]
-
-#     # annotated image is generated with bounding boxes
-#     annotated_image = result.plot()  # use result.plot() to get the annotated image
-        
-
-#     return {"predicted_class": predicted_class_names, "Result": result, "Annotated image": annotated_image}
